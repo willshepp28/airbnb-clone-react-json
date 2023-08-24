330a96483eaa37429ba83fcbf807170735b7620f
willshepp28 <willsheppard29@gmail.com>
2023-08-24 16:02:26 -0500

    updated nav component and created Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { Route, Routes } from 'react-router'
 import './App.css'
 import Nav from './Nav'
+import Home from './Home'
 
 function App() {
   return (
@@ -7,6 +9,9 @@ function App() {
       <h1>Welcome to NookBnb</h1>
       <p>Coming Soon!</p>
       <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Nav(){
     return (
-        <>
-            <h1>Navbar</h1>
-        </>
-    )
-};
+        <nav>
+            <Link to="/">Home</Link>
+        </nav>
+    );
+}
