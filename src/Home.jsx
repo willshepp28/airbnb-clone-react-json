f9ec028e914ef299e555b94cbd2deb4fc0520733
willshepp28 <willsheppard29@gmail.com>
2023-08-24 17:56:16 -0500

    updated title in index.html

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,17 @@
+import { useEffect, useState } from "react";
 
 export default function Home(){
+    const [listings, setListings] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/listings")
+            .then((response) => response.json)
+            .then((data) => console.log(data));
+    });
+
     return (
-        <>
-            <h1>Home</h1>
-        </>
+        <div>
+            <h1>Listings</h1>
+        </div>
     );
 }
